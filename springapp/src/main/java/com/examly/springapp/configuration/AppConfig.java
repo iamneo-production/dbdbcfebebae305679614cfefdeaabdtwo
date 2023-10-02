cbbbe496ae1e30c523b442143879838725d14f14
Iamneo <techadmin@iamneo.ai>
2023-10-02 13:54:30 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/AppConfig.java
@@ -1,4 +1,17 @@
 
-public class AppConfig{
-    
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller{
+    @Value("${test.property}")
+    private String javaHome;
+
+    @GetMapping("/")
+    public String test(){
+
+        return javaHome;
+
+    }
 }
